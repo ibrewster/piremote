1204bde654a18eb695d2703beaf8307f7a87447a
Israel Brewster <israel@brewstersoft.net>
2024-01-04 10:28:39 -0900

    Update service file

--- RunFastWSGI.py
+++ RunFastWSGI.py
@@ -1,11 +1,12 @@
 import paho.mqtt.publish as publish
 
-import fastwsgi
+import bjoern
 from CarBridge import app
 
-fastwsgi.run(
+bjoern.run(
     wsgi_app = app,
     host = '0.0.0.0',
-    port = 5000)
+    port = 5000,
+    reuse_port = True)
 
 publish.single("CarLink/availability", "offline", hostname="watchman.brewstersoft.net")
